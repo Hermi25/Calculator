dccc86025351600492a40293863659af0cc1937b
Hermi25 <jadzia.libera@gmail.com>
2023-06-28 19:29:34 +0200

    Loops - lesson4

---
+++ src/main/java/lesson4/Loops.java
@@ -0,0 +1,22 @@
+package lesson4;
+
+public class Loops {
+    public static void main(String[] args) {
+        int n = 5;
+
+        //loop for
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Java is fun");
+        }
+
+        //loop while
+        int i = 1;
+
+        while (i <= n) {
+            System.out.println("Java is not fun");
+            i++;
+        }
+
+
+    }
+}
